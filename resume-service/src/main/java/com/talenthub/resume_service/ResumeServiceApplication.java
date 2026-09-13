0e020e13ea5a9fa18a881a9889568a0fb94c62b3
Nilupul925 <theekshana.nilupul925@gmail.com>
2026-09-13 23:02:24 +0530

    Add resume service module with initial configuration and application setup

---
+++ resume-service/src/main/java/com/talenthub/resume_service/ResumeServiceApplication.java
@@ -0,0 +1,16 @@
+package com.talenthub.resume_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient 
+public class ResumeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeServiceApplication.class, args);
+		System.out.println("\n__ Resume-Service-Online __");
+	}
+
+}
