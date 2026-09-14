f48b2d8ab2c32602b2e4ade56db9aa2105d19832
bhnandunithathsarani2001 <bhnandunithathsarani2001@gmail.com>
2026-09-14 07:39:32 +0530

    Add EmailNotFoundException class for handling email not found errors

---
+++ profile-service/src/main/java/com/talenthub/profile_service/exception/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.talenthub.profile_service.exception;
+
+public class EmailNotFoundException extends RuntimeException {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
