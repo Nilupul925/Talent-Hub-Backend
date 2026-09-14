f260b92098a7568defbdb3c9f49315944af1fba9
bhnandunithathsarani2001 <bhnandunithathsarani2001@gmail.com>
2026-09-14 07:37:50 +0530

    Add update method to ProfileService for modifying existing profiles

--- profile-service/src/main/java/com/talenthub/profile_service/service/ProfileService.java
+++ profile-service/src/main/java/com/talenthub/profile_service/service/ProfileService.java
@@ -50,5 +50,21 @@ public class ProfileService {
 
         return email + ": Profile deleted successful";
     }
+    public Profile update(String email, Profile request) {
+        Profile existProfile = profileRepository.findByEmail(email)
+           .orElseThrow(() -> new EmailNotFoundException("User Profile is not exist"));
+
+        existProfile.setFirstName(request.getFirstName());
+        existProfile.setLastName(request.getLastName());
+        existProfile.setPhone(request.getPhone());
+        existProfile.setAddress(request.getAddress());
+        existProfile.setAbout(request.getAbout());
+        existProfile.setLinkedin(request.getLinkedin());
+        existProfile.setGithub(request.getGithub());
+        existProfile.setWebsite(request.getWebsite());
+        existProfile.setProfileImageUrl(request.getProfileImageUrl());
+
+        return profileRepository.save(existProfile);
+    }
 
 }
