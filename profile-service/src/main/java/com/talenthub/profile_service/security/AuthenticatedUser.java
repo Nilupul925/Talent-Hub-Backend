944a88ba21b108c77dba4c6a7b8e7563805f9a8b
RaveenaJs <jathungeraveena@gmail.com>
2026-09-14 07:51:35 +0530

    Add AuthenticatedUser class for user authentication representation

---
+++ profile-service/src/main/java/com/talenthub/profile_service/security/AuthenticatedUser.java
@@ -0,0 +1,16 @@
+package com.talenthub.profile_service.security;
+
+import lombok.Data;
+
+@Data 
+public class AuthenticatedUser {
+    private final Integer userId;
+    private final String email;
+    private final String role;
+
+    public AuthenticatedUser(Integer userId, String email, String role) {
+        this.userId = userId;
+        this.email = email;
+        this.role = role;
+    }
+}
