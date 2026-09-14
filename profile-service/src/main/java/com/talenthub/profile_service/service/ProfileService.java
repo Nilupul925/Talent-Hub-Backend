93a892e12c6e9fcf4889bd4aeb2b9da3d33f8fa2
RaveenaJs <jathungeraveena@gmail.com>
2026-09-14 07:16:32 +0530

    Add ProfileService class to handle profile creation and validation

---
+++ profile-service/src/main/java/com/talenthub/profile_service/service/ProfileService.java
@@ -0,0 +1,40 @@
+package com.talenthub.profile_service.service;
+
+import org.springframework.stereotype.Service;
+
+import com.talenthub.profile_service.entity.Profile;
+import com.talenthub.profile_service.repository.ProfileRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service 
+public class ProfileService {
+    private final ProfileRepository profileRepository;
+
+    public ProfileService(ProfileRepository profileRepository){
+        this.profileRepository = profileRepository;
+    }
+
+    public Profile create(String email, Profile request){
+        if(profileRepository.existsByEmail(email)){
+            throw new EmailAlreadyExistsException("This Profile already created");
+        }
+
+        Profile newProfile = new Profile(
+            email, 
+            request.getFirstName(),
+            request.getLastName(),
+            request.getPhone(),
+            request.getAddress(),
+            request.getAbout(),
+            request.getLinkedin(),
+            request.getGithub(),
+            request.getWebsite(),
+            request.getProfileImageUrl()
+        );
+
+        return profileRepository.save(newProfile);
+        
+    }
+
+}
